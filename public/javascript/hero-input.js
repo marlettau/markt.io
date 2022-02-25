1c7d5b650e618fb09e3d20a0d62f326b71b6c8a6
marlettau <dhsrv14@gmail.com>
2022-02-24 23:59:15 -0500

    update

--- public/javascript/hero-input.js
+++ public/javascript/hero-input.js
@@ -1,7 +1,40 @@
 $(function() {
-  $(".hero-stock-input").on("click", function(){
+  $(".hero-stock-input-container").on("click", function(){
     $(this).addClass('hero-input-error');
+    $(this).find("input").addClass('hero-input-error-color-change');
+    $(this).find("i").addClass('hero-stock-input-icon-error');
   }).on("animationend", function(){
     $(this).removeClass('hero-input-error');
+    $(this).find("input").removeClass('hero-input-error-color-change');
+    $(this).find("i").removeClass('hero-stock-input-icon-error');
   });
 });
+
+
+  var menu = document.getElementById("stockSearchInfoDialog")
+
+
+
+  $(".hero-stock-input-icon").on("click", function(){
+
+    $("#stockSearchInfoDialog").fadeIn();
+
+    var offset = $( this ).offset();
+    event.stopPropagation();
+
+    menu.style.display = 'block';
+    menu.style.left = offset.left + "px";
+    menu.style.top = offset.top + "px";
+
+
+    console.log( this.tagName +
+    " coords ( " + offset.left + ", " + offset.top + " )" );
+  });
+
+  $(".stock-search-info-dialog").on("mouseleave", function(){
+    $("#stockSearchInfoDialog").fadeOut();
+
+      //menu.style.display = 'none';
+      $("#stockSearchInfoDialog").removeClass('stock-search-info-dialog-animation');
+
+  });
