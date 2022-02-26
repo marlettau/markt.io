e7422f3265f11c6a975f754c242132043f1d8797
marlettau <dhsrv14@gmail.com>
2022-02-26 01:16:36 -0500

    update

--- app.js
+++ app.js
@@ -2,7 +2,60 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const puppeteer = require('puppeteer');
 
+(async () => {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  const stockSymbol = "MSFT";
+  console.log("Loading Stock Data..");
+  await page.goto("https://finance.yahoo.com/quote/" + stockSymbol);
+
+  //FUNCTIONS
+
+  const getCurrentStockPrice = await page.evaluate(() => {
+    const stock = window.location.href;
+    const stockSymbol = stock.substr(stock.length - 4);
+    const currentStockPrice = document.querySelector(`[data-symbol="${stockSymbol}"][data-field="regularMarketPrice"]`);
+    return currentStockPrice.innerHTML;
+  });
+
+  const getAfterMarketStockPrice = await page.evaluate(() => {
+    const stock = window.location.href;
+    const stockSymbol = stock.substr(stock.length - 4);
+    const afterMarketStockPrice = document.querySelector(`[data-symbol="${stockSymbol}"][data-field="postMarketPrice"]`);
+    return afterMarketStockPrice.innerHTML;
+  });
+
+  const getCurrentPriceChange = await page.evaluate(() => {
+    const stock = window.location.href;
+    const stockSymbol = stock.substr(stock.length - 4);
+    const afterMarketStockPrice = document.querySelector(`[data-symbol="${stockSymbol}"][data-field="regularMarketChange"] span`);
+    return afterMarketStockPrice.innerHTML;
+  });
+
+  const getAfterMarketPriceChange = await page.evaluate(() => {
+    const stock = window.location.href;
+    const stockSymbol = stock.substr(stock.length - 4);
+    const afterMarketStockPrice = document.querySelector(`[data-symbol="${stockSymbol}"][data-field="postMarketChange"] span`);
+    return afterMarketStockPrice.innerHTML;
+  });
+
+
+
+
+
+
+  //OUTPUT
+
+  console.log("Stock Data for: " + stockSymbol);
+  console.log("Current Stock price is: " + getCurrentStockPrice);
+  console.log("The Current Price change is: " + getCurrentPriceChange);
+  console.log("After Market Stock price is: " + getAfterMarketStockPrice);
+  console.log("After Market Price change is: " + getAfterMarketPriceChange);
+
+  await browser.close();
+})();
 
 //STATIC IMPORTING
 app.use(express.static('public'));
