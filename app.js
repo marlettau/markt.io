355cdcb86efa513b3b877eab05dcfeee8d7ed6fd
marlettau <dhsrv14@gmail.com>
2022-02-27 04:12:02 -0500

    update

--- app.js
+++ app.js
@@ -14,14 +14,74 @@ let stockSymbol = "";
 
 async function stocks()
 {
-  const browser = await puppeteer.launch({headless: true});
+  const blocked_domains = [
+  'googlesyndication.com',
+  'adservice.google.com',
+];
+
+
+  const minimal_args = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+];
+
+  const browser = await puppeteer.launch({headless: true, args: minimal_args});
   const page = await browser.newPage();
   console.log("Loading Stock Data..");
-  await page.goto("https://finance.yahoo.com/quote/" + stockSymbol);
+
+
+await page.setRequestInterception(true);
+page.on('request', (req) => {
+const url = req.url();
+if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image' || blocked_domains.some(domain => url.includes(domain))){
+req.abort();
+}
+else {
+req.continue();
+}
+});
+
+
+  await page.goto("https://www.yahoofinance.com/quote/" + stockSymbol);
 
   //FUNCTIONS
-
   try {
+
+
+
     const getCurrentStockPrice = await page.evaluate(() => {
       const stock = window.location.href;
       const stockSymbol = stock.substring(stock.indexOf('quote/')+6, stock.length);
@@ -54,12 +114,10 @@ async function stocks()
 
 
 
+return getCurrentStockPrice;
 
-
-
-    return getCurrentStockPrice;
   } catch (e) {
-    console.log("Not a real stock");
+    console.log("Not a real stock" + e);
     return "notValid";
   } finally {
 
